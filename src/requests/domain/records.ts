460bd17c3b9928f8ea7919f906ea77dda8f95549
James Wylie <9528193+jameswylie@users.noreply.github.com>
2024-01-16 21:28:13 +0000

    feat: request records.

--- src/requests/domain/records.ts
+++ src/requests/domain/records.ts
@@ -3,71 +3,66 @@ import { InstancePropsI } from "../../common/types";
 import { domainToNonFungId } from "../../utils/domain.utils";
 import { RecordItem } from "../../mappings/records";
 
-export async function requestRecords(domainName: string, instance: InstancePropsI) {
+export async function requestRecords(domainName: string, { state, entities }: InstancePropsI) {
 
     const domainId = await domainToNonFungId(domainName);
 
-
-    const recordsVaultId = ((await instance.state.innerClient.keyValueStoreData({
+    const recordsVaultId = ((await state.innerClient.keyValueStoreData({
         stateKeyValueStoreDataRequest: {
-            key_value_store_address: instance.entities.recordServiceVaultId,
+            key_value_store_address: entities.recordServiceVaultId,
             keys: [{ key_json: { kind: 'NonFungibleLocalId', value: domainId } }]
         }
     })).entries[0]?.value.programmatic_json as ProgrammaticScryptoSborValueOwn)?.value;
 
-    console.log('domain:', domainId);
-    console.log('service vault id:', instance.entities.recordServiceVaultId);
-    console.log('record vault id:', recordsVaultId);
+    if (!recordsVaultId) {
+        return [];
+    }
 
-    // if (!recordsVaultId) {
-    //     return [];
-    // }
+    const recordIds = (await state.innerClient.entityNonFungibleIdsPage({
+        stateEntityNonFungibleIdsPageRequest: {
+            address: entities.rnsStorage,
+            resource_address: entities.resolverRecordResource,
+            vault_address: recordsVaultId,
+        }
+    })).items;
 
-    // const recordIds = (await instance.state.innerClient.entityNonFungibleIdsPage({
-    //     stateEntityNonFungibleIdsPageRequest: {
-    //         address: instance.entities.rnsStorage,
-    //         resource_address: instance.entities.resolverRecordResource,
-    //         vault_address: recordsVaultId,
-    //     }
-    // })).items;
+    return (await state.getNonFungibleData(entities.resolverRecordResource, recordIds))
+        .map(nft => {
+            if (nft.data?.programmatic_json.kind === 'Tuple') {
+                return nft.data?.programmatic_json.fields.reduce((acc, field) => {
+                    if (field.type_name === 'RecordType' && field.kind === 'Enum') {
+                        if (field.field_name) {
+                            return { ...acc, [field.field_name]: field.variant_name }
+                        }
+                    }
 
-    // return (await instance.state.getNonFungibleData(instance.entities.resolverRecordResource, recordIds))
-    //     .map(nft => {
-    //         if (nft.data?.programmatic_json.kind === 'Tuple') {
-    //             return nft.data?.programmatic_json.fields.reduce((acc, field) => {
-    //                 if (field.type_name === 'RecordType' && field.kind === 'Enum') {
-    //                     if (field.field_name) {
-    //                         return { ...acc, [field.field_name]: field.variant_name }
-    //                     }
-    //                 }
+                    if (field.type_name === 'Option' && field.kind === 'Enum' && field.field_name !== 'value') {
+                        if (field.field_name) {
+                            const value = (field.fields.length && 'value' in field.fields[0]) ? field.fields[0].value : null;
 
-    //                 if (field.type_name === 'Option' && field.kind === 'Enum' && field.field_name !== 'value') {
-    //                     if (field.field_name) {
-    //                         const value = (field.fields.length && 'value' in field.fields[0]) ? field.fields[0].value : null;
+                            return { ...acc, [field.field_name]: value }
+                        }
+                    }
 
-    //                         return { ...acc, [field.field_name]: value }
-    //                     }
-    //                 }
+                    if (field.field_name === 'id_additions' && field.kind === 'Array') {
+                        return { ...acc, [field.field_name]: field.elements.map(e => ('value' in e && e.value) as string) }
+                    }
 
-    //                 if (field.field_name === 'id_additions' && field.kind === 'Array') {
-    //                     return { ...acc, [field.field_name]: field.elements.map(e => ('value' in e && e.value) as string) }
-    //                 }
+                    if (field.field_name === 'value' && field.kind === 'Enum') {
+                        const value = (('fields' in field.fields[0] && 'value' in field.fields[0].fields[0] && field.fields[0].fields[0].value) || null) as string | null;
 
-    //                 if (field.field_name === 'value' && field.kind === 'Enum') {
-    //                     const value = (('fields' in field.fields[0] && 'value' in field.fields[0].fields[0] && field.fields[0].fields[0].value) || null) as string | null;
+                        return { ...acc, [field.field_name]: value }
+                    }
 
-    //                     return { ...acc, [field.field_name]: value }
-    //                 }
-
-    //                 if (field.kind === 'String' || field.kind === 'NonFungibleLocalId') {
-    //                     if (field.field_name) {
-    //                         return { ...acc, [field.field_name]: field.value };
-    //                     }
-    //                 }
-    //                 return acc;
-    //             }, { record_id: nft.non_fungible_id } as RecordItem)
-    //         }
-    //         return {} as RecordItem;
-    //     });
+                    if (field.kind === 'String' || field.kind === 'NonFungibleLocalId') {
+                        if (field.field_name) {
+                            return { ...acc, [field.field_name]: field.value };
+                        }
+                    }
+                    return acc;
+                }, { record_id: nft.non_fungible_id } as RecordItem)
+            }
+            return {} as RecordItem;
+        });
 
 }
