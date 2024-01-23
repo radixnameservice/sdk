93e1a68e3d364154f7754cdda26f20f0677ec1b7
James Wylie <9528193+jameswylie@users.noreply.github.com>
2024-01-23 17:14:25 +0000

    feat: verbose status outputs.

--- src/mappings/status.ts
+++ src/mappings/status.ts
@@ -1,16 +1,40 @@
-export function mapStatusInt(status: number) {
+export function mapStatusInt(domain: string, status: number) {
 
     const statusMaps: any = {
-        '0': 'available',
-        '1': 'settlement',
-        '2': 'auction',
-        '3': 'taken',
-        '4': 'sunrise',
-        '5': 'tld',
-        '6': 'genus-substrate'
+        '0': {
+            status: 'available',
+            verbose: `${domain} is available.`
+        },
+        '1': {
+            status: 'settlement',
+            verbose: `${domain} is currently within its settlement period.`
+        },
+        '2': {
+            status: 'auction',
+            verbose: `${domain} is currently within an auction.`
+        },
+        '3': {
+            status: 'taken',
+            verbose: `${domain} is taken.`
+        },
+        '4': {
+            status: 'sunrise',
+            verbose: `${domain} is protected by the RNS Foundation as part of the Sunrise Initiative.`
+        },
+        '5': {
+            status: 'tld',
+            verbose: `${domain} is protected by the RNS Foundation as part of Project TLD.`
+        },
+        '6': {
+            status: 'genus-substrate',
+            verbose: `${domain} is protected by the RNS Foundation as part of the genus substrate program.`
+        }
     }
 
-    return statusMaps[status] ?? 'unknown';
+    return statusMaps[status] ?? {
+        status: 'unknown',
+        verbose: `The status of ${domain} is unknown. It's possible that the queried domain is invalid.`
+    };
 
 }
 
