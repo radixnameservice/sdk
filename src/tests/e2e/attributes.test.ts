d23de6f4c74e473d9917ef7b2276cd17bfec12a0
James Wylie <9528193+jameswylie@users.noreply.github.com>
2024-02-28 17:21:23 +0000

    feat: re-enabled tests.

---
+++ src/tests/e2e/attributes.test.ts
@@ -0,0 +1,23 @@
+import RnsKit from '../..';
+import { matchObjectTypes } from '../utils';
+
+const attributesSchema = {
+    status: 'string',
+    verbose: 'string'
+};
+
+
+describe('RnsKit', () => {
+
+    const rns = new RnsKit({ network: 'stokenet' });
+
+    it(`should return a 'registered' or 'settltment' status.`, async () => {
+
+        const attributes = await rns.getDomainAttributes('radixnameservice.xrd');
+
+        expect(matchObjectTypes(attributes, attributesSchema)).toBe(true);
+        expect(attributes.status).not.toBe('available');
+
+    });
+
+});
