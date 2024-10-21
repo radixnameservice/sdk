8d2b7d5f7a2a4b184c52ea814ac8124488661b71
James Wylie <9528193+jameswylie@users.noreply.github.com>
2024-10-21 19:35:58 +0100

    chore: consolidated domain pricing into attributes test.

--- src/tests/e2e/attributes.test.ts
+++ src/tests/e2e/attributes.test.ts
@@ -1,9 +1,12 @@
 import RnsKit from '../..';
+import { convertToDecimal } from '../../utils/decimal.utils';
 import { matchObjectTypes } from '../utils';
+import Decimal from 'decimal.js';
 
 const attributesSchema = {
     status: 'string',
-    verbose: 'string'
+    verbose: 'string',
+    price: 'object'
 };
 
 
@@ -11,12 +14,19 @@ describe('RnsKit', () => {
 
     const rns = new RnsKit({ network: 'stokenet' });
 
-    it(`should return a 'registered' or 'settltment' status.`, async () => {
+    it(`should return a 'registered' or 'settlement' status and decimal / numerical denoted price.`, async () => {
 
         const attributes = await rns.getDomainAttributes('radixnameservice.xrd');
 
         expect(matchObjectTypes(attributes, attributesSchema)).toBe(true);
         expect(attributes.status).not.toBe('available');
+
+        expect(typeof attributes.price === 'object' && typeof attributes.price.usd === 'number' && (typeof attributes.price.xrd === 'number' || typeof attributes.price.xrd === 'object')).toBe(true);
+        
+        const xrdPriceDecimal = convertToDecimal(attributes.price.xrd);
+        expect(xrdPriceDecimal).toBeInstanceOf(Decimal);
+        expect(xrdPriceDecimal.toNumber()).toBeGreaterThan(0);
+        expect(attributes.price.usd).toBe(4);
 
     });
 
