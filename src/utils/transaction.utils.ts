2737364f64e7df1569c3ee40d3822873029d19be
James Wylie <9528193+jameswylie@users.noreply.github.com>
2024-10-30 22:22:52 +0000

    chore: extracted types and utils.

--- src/utils/transaction.utils.ts
+++ src/utils/transaction.utils.ts
@@ -1,26 +1,4 @@
-import { Transaction } from "@radixdlt/babylon-gateway-api-sdk";
-import { RadixDappToolkit } from "@radixdlt/radix-dapp-toolkit";
-
-interface SendTransactionI {
-
-    id: string;
-    manifest: string;
-    rdt: RadixDappToolkit;
-    transaction: Transaction;
-    callbacks?: EventCallbacksI;
-    message?: string;
-
-}
-
-export interface EventCallbacksI {
-
-    onInit?: Function;
-    onSuccess?: Function;
-    onFail?: Function;
-    onAppApproved?: Function;
-    onIntentReceipt?: Function;
-
-}
+import { SendTransactionI } from "../common/transaction.types";
 
 export async function sendTransaction({ manifest, rdt, transaction, callbacks, message }: SendTransactionI): Promise<boolean> {
 
