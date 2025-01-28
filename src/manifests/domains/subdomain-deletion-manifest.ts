d62b0a9418aa8e086ea51258477ac44092ee881c
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-01-28 20:30:10 +0000

    feat: subdomain removal manifest.

---
+++ src/manifests/domains/subdomain-deletion-manifest.ts
@@ -0,0 +1,35 @@
+import RnsSDK from "../..";
+import { UserSpecificsI } from "../../common/user.types";
+
+export default async function subdomainCreationManifest({
+    sdkInstance,
+    userDetails,
+    rootDomainId,
+    subdomainId,
+}: {
+    sdkInstance: RnsSDK;
+    userDetails: UserSpecificsI;
+    rootDomainId: string;
+    subdomainId: string;
+}) {
+
+    return `
+        CALL_METHOD
+            Address("${userDetails.accountAddress}")
+            "create_proof_of_non_fungibles"
+            Address("${sdkInstance.entities.resources.collections.domains}")
+            Array<NonFungibleLocalId>(
+                NonFungibleLocalId("${rootDomainId}")
+            );
+        POP_FROM_AUTH_ZONE
+            Proof("requester_proof");
+        CALL_METHOD
+            Address("${sdkInstance.entities.components.coreVersionManager.rnsCoreComponent}")
+            "delete_subdomain"
+            NonFungibleLocalId("${subdomainId}")
+            Proof("requester_proof")
+            Enum<0u8>();
+        DROP_ALL_PROOFS;
+    `;
+    
+}
