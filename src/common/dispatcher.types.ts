c341b884266e5b39b9fdbc661e896b16647d3c2a
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-01-28 01:20:12 +0000

    feat: added proof types.

--- src/common/dispatcher.types.ts
+++ src/common/dispatcher.types.ts
@@ -2,7 +2,7 @@ import { RadixDappToolkit } from "@radixdlt/radix-dapp-toolkit";
 
 import { UserSpecificsI } from "./user.types";
 import { EventCallbacksI } from "./transaction.types";
-import { InstancePropsI } from "./entities.types";
+import { InstancePropsI, ProofsI } from "./entities.types";
 import { DocketI } from "./record.types";
 
 export interface DispatcherPropsI extends InstancePropsI {
@@ -29,8 +29,9 @@ export interface RegistrationDispatcherPropsI extends DispatcherPropsI {
 export interface CreateRecordDispatcherPropsI extends DispatcherPropsI {
    
     userDetails: UserSpecificsI;
-    domainId: string;
     rootDomainId: string;
+    subDomainId?: string;
     docket: DocketI;
+    proofs?: ProofsI;
 
 }
