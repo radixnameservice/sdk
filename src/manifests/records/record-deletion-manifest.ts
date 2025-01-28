0c57e4bf0cac8b122608f69fd0eba99d417e1b56
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-01-28 20:28:00 +0000

    feat: record removal manifest.

---
+++ src/manifests/records/record-deletion-manifest.ts
@@ -0,0 +1,38 @@
+import RnsSDK from "../..";
+import { UserSpecificsI } from "../../common/user.types";
+
+export function recordCreationManifest({
+    sdkInstance,
+    userDetails,
+    rootDomainId,
+    recordId
+}: {
+    sdkInstance: RnsSDK;
+    userDetails: UserSpecificsI;
+    rootDomainId: string;
+    recordId: string;
+}): string {
+
+    return `
+        CALL_METHOD
+            Address("${userDetails.accountAddress}")
+            "create_proof_of_non_fungibles"
+            Address("${sdkInstance.entities.resources.collections.domains}")
+            Array<NonFungibleLocalId>(
+                NonFungibleLocalId("${rootDomainId}")
+            );
+        POP_FROM_AUTH_ZONE
+            Proof("requester_proof");
+        CALL_METHOD
+            Address("${sdkInstance.entities.components.coreVersionManager.rnsCoreComponent}")
+            "delete_record"
+            NonFungibleLocalId("${recordId}")
+            Proof("requester_proof")
+            Enum<0u8>();
+        CALL_METHOD
+            Address("${userDetails.accountAddress}")
+            "deposit_batch"
+            Expression("ENTIRE_WORKTOP");
+    `;
+
+}
