3b445835b519514338136d35c2b92f5b99bf9aab
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-01-30 02:32:36 +0000

    feat: domain activation manifest.

---
+++ src/manifests/domains/domain-activation-manifest.ts
@@ -0,0 +1,54 @@
+import RnsSDK from "../..";
+import { UserSpecificsI } from "../../common/user.types";
+
+export default async function registerDomainManifest({
+    sdkInstance,
+    userDetails,
+    rootDomainId,
+    subdomainIds,
+}: {
+    sdkInstance: RnsSDK;
+    userDetails: UserSpecificsI;
+    rootDomainId: string;
+    subdomainIds: string[];
+}) {
+
+    return `
+        CALL_METHOD
+            Address("${userDetails.accountAddress}")
+            "create_proof_of_non_fungibles"
+            Address("${sdkInstance.entities.resources.collections.domains}")
+            Array<NonFungibleLocalId>(
+                NonFungibleLocalId("${rootDomainId}")
+            );
+        POP_FROM_AUTH_ZONE
+            Proof("domain_proof");
+        CALL_METHOD
+            Address("${sdkInstance.entities.components.coreVersionManager.rnsCoreComponent}")
+            "update_address"
+            Proof("domain_proof")
+            Address("${userDetails.accountAddress}");
+        CALL_METHOD
+            Address("${userDetails.accountAddress}")
+            "create_proof_of_non_fungibles"
+            Address("${sdkInstance.entities.resources.collections.domains}")
+            Array<NonFungibleLocalId>(
+                NonFungibleLocalId("${rootDomainId}")
+            );
+        POP_FROM_AUTH_ZONE
+            Proof("requested_proof");
+        CALL_METHOD
+            Address("${sdkInstance.entities.components.coreVersionManager.rnsCoreComponent}")
+            "delete_all_records_and_subdomains"
+            NonFungibleLocalId("${rootDomainId}")
+            true
+            true
+            Array<NonFungibleLocalId>(
+                ${subdomainIds.map(id => `NonFungibleLocalId("${id}")`).join(', ')}
+            )
+            Proof("requested_proof")
+            Enum<0u8>();
+        DROP_ALL_PROOFS;
+    `;
+
+}
