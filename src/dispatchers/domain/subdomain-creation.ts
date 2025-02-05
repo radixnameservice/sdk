85785b8df44f801a7d43ab29f66fe4022188623a
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-02-05 23:12:20 +0000

    feat: added subdomain scaffolding.

---
+++ src/dispatchers/domain/subdomain-creation.ts
@@ -0,0 +1,37 @@
+import activateDomainManifest from "../../manifests/domains/domain-activation-manifest";
+
+import { sendTransaction } from "../../utils/transaction.utils";
+import { errorResponse, successResponse } from "../../utils/response.utils";
+import { activationErrors } from "../../common/errors";
+
+import { ActivationDispatcherPropsI } from "../../common/dispatcher.types";
+import { ErrorStackResponse, CommitmentStackResponse } from "../../common/response.types";
+
+
+export async function dispatchSubdomainCreation({
+    sdkInstance,
+    domain,
+    rdt,
+    userDetails,
+    callbacks
+}: ActivationDispatcherPropsI): Promise<CommitmentStackResponse | ErrorStackResponse> {
+
+    try {
+
+        const details = await sdkInstance.getDomainDetails(domain);
+
+        if ('errors' in details) {
+            return details;
+        }
+        return successResponse({
+            code: 'SUBDOMAIN_CREATION_SUCCESSFUL',
+            details: `${domain} was succesfully activated.`
+        });
+
+    } catch (error) {
+
+        return errorResponse(activationErrors.generic({ domain, verbose: error }));
+
+    }
+
+}
