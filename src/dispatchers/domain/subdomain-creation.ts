855ea16b507e9aef758f2c5617aa1ebff292095a
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-02-06 01:18:07 +0000

    feat: domain normalisation and root ID derivation.

--- src/dispatchers/domain/subdomain-creation.ts
+++ src/dispatchers/domain/subdomain-creation.ts
@@ -1,37 +1,51 @@
-import activateDomainManifest from "../../manifests/domains/domain-activation-manifest";
+import subdomainCreationManifest from "../../manifests/domains/subdomain-creation-manifest";
 
 import { sendTransaction } from "../../utils/transaction.utils";
 import { errorResponse, successResponse } from "../../utils/response.utils";
+import { deriveRootDomain, normaliseDomain } from "../../utils/domain.utils";
+
 import { activationErrors } from "../../common/errors";
 
-import { ActivationDispatcherPropsI } from "../../common/dispatcher.types";
+import { SubdomainDispatcherPropsI } from "../../common/dispatcher.types";
 import { ErrorStackResponse, CommitmentStackResponse } from "../../common/response.types";
+
 
 
 
 export async function dispatchSubdomainCreation({
     sdkInstance,
-    domain,
+    subdomain,
     rdt,
     userDetails,
     callbacks
-}: ActivationDispatcherPropsI): Promise<CommitmentStackResponse | ErrorStackResponse> {
+}: SubdomainDispatcherPropsI): Promise<CommitmentStackResponse | ErrorStackResponse> {
 
     try {
-        
-        const details = await sdkInstance.getDomainDetails(domain);
+
+        const normalisedSubDomain = normaliseDomain(subdomain);
+        const rootDomain = deriveRootDomain(normalisedSubDomain);
+
+        const details = await sdkInstance.getDomainDetails(rootDomain);
 
         if ('errors' in details) {
             return details;
         }
+
+        const manifest = await subdomainCreationManifest({
+            sdkInstance,
+            subdomain: normalisedSubDomain,
+            rootDomainId: details.id,
+            userDetails
+        });
+
         return successResponse({
             code: 'SUBDOMAIN_CREATION_SUCCESSFUL',
-            details: `${domain} was succesfully activated.`
+            details: `${normalisedSubDomain} was succesfully created.`
         });
 
     } catch (error) {
 
-        return errorResponse(activationErrors.generic({ domain, verbose: error }));
+       // return errorResponse(activationErrors.generic({ domain, verbose: error }));
 
     }
 
