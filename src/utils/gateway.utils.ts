5cac6409c9dc19f58f7f954896769be62ff9c802
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-02-10 23:58:04 +0000

    chore: extracted gateway utils and enums into dedicated files.

--- src/utils/gateway.utils.ts
+++ src/utils/gateway.utils.ts
@@ -1,12 +1,9 @@
 import { NetworkConfigurationResponseWellKnownAddresses, Status } from "@radixdlt/babylon-gateway-api-sdk";
 import { ucWords } from "./string.utils";
+import { Network } from "../mappings/gateway";
 
-export enum Network {
-    Mainnet = 'https://mainnet.radixdlt.com',
-    Stokenet = 'https://stokenet.radixdlt.com'
-}
+import { NetworkT } from "../common/gateway.types";
 
-export type NetworkT = 'mainnet' | 'stokenet';
 
 export function getBasePath(network: NetworkT = 'mainnet') {
     return Network[ucWords(network)];
