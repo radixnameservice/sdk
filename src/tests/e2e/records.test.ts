a0b595a876ec6540a8e5b72fec3f01d1a7de2255
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-02-10 17:56:17 +0000

    test: domain record creation.

--- src/tests/e2e/records.test.ts
+++ src/tests/e2e/records.test.ts
@@ -1,14 +1,28 @@
+import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit';
+import { RadixNetwork } from '@radixdlt/babylon-gateway-api-sdk';
 import RnsSDK, { RecordItem, ResolvedRecordResponse } from '../..';
-import { matchObjectTypes } from '../utils';
+import { matchObjectTypes, normaliseManifest } from '../utils';
+import { DocketI } from '../../common/record.types';
 
 const mocks = {
+    domain: {
+        name: "radixnameservice.xrd"
+    },
     userDetails: {
-        accountAddress: 'account_tdx_2_129076yrjr5k4lumhp3fl2r88xt3eqgxwed6saplvf2ezz5szrhet8k',
+        accountAddress: 'account_tdx_2_128jmkhrkxwd0h9vqfetw34ars7msls9kmk5y60prxsk9guwuxskn5p',
         badgeId: '#1'
     },
     callbacks: {},
     intentHash: 'txid_tdx_2_1p9j7njn5wuagry6j8mrmkvhhwvttskq2cy4e5nk2wpexhqjav2dszpptsr'
 };
+
+const anticipated = {
+    domain: {
+        rootId: "[52e57ee0bdd7681786e15a0dabb7bdc4]",
+        name: "radixnameservice.xrd",
+        subdomainIds: ["[0601f34183fb940a690f3c87f29b6c25]"]
+    }
+}
 
 jest.mock('@radixdlt/radix-dapp-toolkit', () => {
     return {
@@ -105,7 +119,73 @@ describe('RNS - Fetch Domain Records', () => {
 
 });
 
-// describe('RNS - Create a Domain Record', () => {
+describe('RNS - Create Domain Record', () => {
 
-    
-// });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    const dAppToolkit = RadixDappToolkit({
+        dAppDefinitionAddress: 'account_tdx_2_129076yrjr5k4lumhp3fl2r88xt3eqgxwed6saplvf2ezz5szrhet8k',
+        networkId: RadixNetwork.Stokenet
+    });
+
+    const rns = new RnsSDK({ network: 'stokenet', rdt: dAppToolkit });
+
+    it(`should return a correctly formatted manifest string`, async () => {
+
+        const docket = {
+            context: "receivers",
+            directive: "*",
+            platformIdentifier: "SDK Tests",
+            value: mocks.userDetails.accountAddress
+        } as DocketI;
+
+        const createRecord = await rns.createRecord({
+            domain: mocks.domain.name,
+            userDetails: mocks.userDetails,
+            docket
+        });
+
+        if ('errors' in createRecord) {
+            throw new Error('Mock registration failed');
+        }
+
+        const sendTransactionMock = dAppToolkit.walletApi.sendTransaction as jest.Mock;
+        expect(sendTransactionMock).toHaveBeenCalled();
+
+        const sendTransactionArgs = sendTransactionMock.mock.calls[0][0];
+        const transactionManifest = sendTransactionArgs.transactionManifest;
+
+        const expectedString = `
+            CALL_METHOD
+                Address("${mocks.userDetails.accountAddress}")
+                "create_proof_of_non_fungibles"
+                Address("${rns.entities.resources.collections.domains}")
+                Array<NonFungibleLocalId>(
+                NonFungibleLocalId("${anticipated.domain.rootId}")
+                );
+            POP_FROM_AUTH_ZONE
+                Proof("request_proof");
+            CALL_METHOD
+                Address("${rns.entities.components.coreVersionManager.rnsCoreComponent}")
+                "create_record"
+                NonFungibleLocalId("${anticipated.domain.rootId}")
+                "${docket.context}"
+                ${docket.directive}
+                ${docket.platformIdentifier}
+                Array<String>()
+                Proof("request_proof")
+                Enum<0u8>();
+            CALL_METHOD
+                Address("${mocks.userDetails.accountAddress}")
+                "deposit_batch"
+                Expression("ENTIRE_WORKTOP");
+        `;
+
+        expect(normaliseManifest(transactionManifest)).toBe(normaliseManifest(expectedString));
+
+    });
+
+
+});
