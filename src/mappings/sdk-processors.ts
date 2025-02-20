235da624c7e4c931338dc65be8df33416991bf1c
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-02-20 02:54:01 +0000

    feat: subdomain param processor.

--- src/mappings/sdk-processors.ts
+++ src/mappings/sdk-processors.ts
@@ -1,13 +1,19 @@
-import errors from "./errors";
-import { normaliseDomain, validateDomainEntity } from "../utils/domain.utils";
+import errors, { subdomain } from "./errors";
+import { normaliseDomain, validateDomain, validateSubdomain } from "../utils/domain.utils";
 import { ParamProcessMapT } from "../common/validation.types";
 
 export const parameterProcessMap: ParamProcessMapT = {
 
     domain: {
         normalize: normaliseDomain,
-        validate: validateDomainEntity,
+        validate: validateDomain,
         missingError: errors.domain.generic
+    },
+
+    subdomain: {
+        normalize: normaliseDomain,
+        validate: validateSubdomain,
+        missingError: errors.subdomain.generic
     },
 
 };
