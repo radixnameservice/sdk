bb1b313ec90bd18d651d749948a34768cd1e922a
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-03-14 12:31:52 +0000

    feat: provide improved build support for imports.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/index.ts"],
     format: ["cjs", "esm"],
+    outExtension({ format }) {
+        return format === "esm"
+            ? { js: ".mjs" }
+            : { js: ".cjs" };
+    },
     dts: true,
     splitting: false,
     sourcemap: true,
