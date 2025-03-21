e7754ba8708b8836414ec305c2804705c440634c
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-03-21 20:30:49 +0000

    feat: decorator instance dep injection.

--- src/mappings/sdk-processors.ts
+++ src/mappings/sdk-processors.ts
@@ -1,6 +1,7 @@
+import RnsSDK from "..";
 import errors from "./errors";
 import { normaliseDomain, validateDomain, validateSubdomain } from "../utils/domain.utils";
-import { validateAddress } from "../utils/address.utils";
+import { validateAccountAddress } from "../utils/address.utils";
 import { ParamProcessMapT } from "../common/validation.types";
 
 export const parameterProcessMap: ParamProcessMapT = {
@@ -19,7 +20,7 @@ export const parameterProcessMap: ParamProcessMapT = {
 
     accountAddress: {
         normalize: (accountAddress: string) => accountAddress.toLowerCase(),
-        validate: validateAddress,
+        validate: (accountAddress: string, instance: RnsSDK) => validateAccountAddress(accountAddress, { network: instance.network }),
         missingError: errors.account.invalidAddress
     },
 
