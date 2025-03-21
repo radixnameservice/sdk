94ef5b1c888c7928c1f6a09fb0d5c0422f4af13a
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-03-21 20:31:53 +0000

    feat: enabled param processor sdk instance access.

--- src/common/validation.types.ts
+++ src/common/validation.types.ts
@@ -1,9 +1,10 @@
+import RnsSDK from "..";
 import { ErrorI } from "./response.types";
 
 export type ParamProcessConfigT = {
-    normalize?: (value: any) => any | Promise<any>;
-    validate?: (value: any) => true | ErrorI | Promise<true | ErrorI>;
-    missingError?: (value: any) => ErrorI;
+    normalize?: (value: any, instance: RnsSDK) => any | Promise<any>;
+    validate?: (value: any, instance: RnsSDK) => true | ErrorI | Promise<true | ErrorI>;
+    missingError?: (value: any, instance: RnsSDK) => ErrorI;
 };
 
 export type ParamProcessMapT = {
