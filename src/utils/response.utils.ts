de54ca8f3c3a0b01818d1bdb366d9ec1d6ec6158
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-03-27 20:14:10 +0000

    feat: response wrapper util.

--- src/utils/response.utils.ts
+++ src/utils/response.utils.ts
@@ -31,3 +31,31 @@ export function errorStack(errors: ErrorI | ErrorI[]): ErrorStackI {
     };
 
 }
+
+export function wrapResponse<T>(response: T | ErrorStackI): T & { errors?: ErrorI[] } {
+
+    const isError = response && typeof response === "object" && "errors" in response && Array.isArray((response as any).errors);
+
+    if (Array.isArray(response)) {
+
+        const newArray = response.slice();
+
+        Object.defineProperty(newArray, "errors", {
+            value: isError ? (response as any).errors : undefined,
+            enumerable: false, // so it won't show up in array iterations
+            configurable: true,
+            writable: false,
+        });
+
+        return newArray as T & { errors?: ErrorI[] };
+    } else {
+
+        const newObj = { ...(response as object) } as T & { errors?: ErrorI[] };
+
+        if (!isError) {
+            newObj.errors = undefined;
+        }
+
+        return newObj;
+    }
+}
