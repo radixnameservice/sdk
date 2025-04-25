ec6c25f0b2e68da82ad948ca5256e33ff5bdc227
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-04-25 18:43:57 +0100

    feat: docket to ID universal record formatting.

--- src/utils/record.utils.ts
+++ src/utils/record.utils.ts
@@ -2,13 +2,13 @@ import { DocketPropsI } from "../common/record.types";
 import { domainToNonFungId } from "./domain.utils";
 
 
-export async function docketToRecordId(domain: string, docket: DocketPropsI, includeAdditions?: boolean) {
+export async function docketToRecordId(domain: string, docket: DocketPropsI) {
 
     const domainId = await domainToNonFungId(domain);
     const parsedContext = docket.context ? `-${docket.context}` : '';
     const parsedDirective = docket.directive ? `-${docket.directive}` : '';
 
-    const recordId = await domainToNonFungId(`${domainId}${parsedContext}${parsedDirective}${includeAdditions ? '-' : ''}`);
+    const recordId = await domainToNonFungId(`${domainId}${parsedContext}${parsedDirective}`);
 
     return recordId;
 
